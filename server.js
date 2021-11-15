d12a8a283c1a11c303f3eb364e42dc85fbad9484
Donovan K <7donovan7@gmail.com>
2021-11-14 21:19:52 -0500

    html routing

--- server.js
+++ server.js
@@ -2,11 +2,20 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const routes = require('./routes/htmlRoutes');
+// const apiRoute = require("./routes/apiRoutes");
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/workout')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", routes);
+// app.use('/api', apiRoute);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
